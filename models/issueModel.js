8ce465bcd5a26ab97efe94e3540d1f9e96860380
Rafał Kowalczyk <raff1410X@gmail.com>
2022-11-15 18:50:11 +0100

    Add Error handler for required fields

--- models/issueModel.js
+++ models/issueModel.js
@@ -5,17 +5,17 @@ const issueSchema = new mongoose.Schema(
     {
         issue_title: {
             type: String,
-            required: [true, 'A issue must have a title'],
+            required: [true, 'required field(s) missing'],
         },
         issue_text: {
             type: String,
-            required: [true, 'A issue must have a description'],
+            required: [true, 'required field(s) missing'],
         },
         created_on: { type: Date, default: new Date() },
         updated_on: { type: Date, default: new Date() },
         created_by: {
             type: String,
-            required: [true, 'A issue must have an author'],
+            required: [true, 'required field(s) missing'],
         },
         assigned_to: { type: String, default: '' },
         open: { type: Boolean, default: true },
