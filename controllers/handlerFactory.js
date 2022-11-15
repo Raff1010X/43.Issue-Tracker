aae88bc64661794aa4758489f52ba36e1a8b0be2
Rafał Kowalczyk <raff1410X@gmail.com>
2022-11-15 21:47:04 +0100

    Add updateOne and deleteOne rules do implement

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -24,6 +24,10 @@ exports.updateOne = () =>
             { updated_on: new Date(), ...req.body }
         );
         res.status(200).json(doc);
+        // { result: 'successfully updated', '_id': _id }
+        // If not include an _id, the return value is { error: 'missing _id' }.
+        // If not include update fields, the return value is { error: 'no update field(s) sent', '_id': _id }. 
+        // On any other error, the return value is { error: 'could not update', '_id': _id }.
     });
 
 exports.deleteOne = () =>
@@ -31,4 +35,7 @@ exports.deleteOne = () =>
         const Model = mongoose.model(req.params.project, issueSchema);
         let doc = await Model.deleteOne({ _id: req.body._id });
         res.status(200).json(doc);
+        // If no _id is sent, the return value is { error: 'missing _id' }. 
+        // On success, the return value is { result: 'successfully deleted', '_id': _id }. 
+        // On failure, the return value is { error: 'could not delete', '_id': _id }.
     });
